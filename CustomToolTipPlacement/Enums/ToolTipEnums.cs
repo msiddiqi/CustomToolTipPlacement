9605f925c6612c49ccc848a5dc5b02589b6ca342
msiddiqi <shujaatned@hotmail.com>
2014-03-08 12:12:34 -0500

    Added code for custom placement behavior

---
+++ CustomToolTipPlacement/Enums/ToolTipEnums.cs
@@ -0,0 +1,13 @@
+﻿
+namespace CustomToolTipPlacement.Enums
+{
+    public enum HorizontalPlacement
+    {
+        Left = 0, Center = 1, Right = 2
+    }
+
+    public enum VerticalPlacement
+    {
+        Top = 0, Center = 1, Bottom = 2
+    }
+}
